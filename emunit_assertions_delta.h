4639777a804d83e3340d2bbe236a00cdc040b2df
Radosław Koppel <r.koppel@k-el.com>
2017-03-27 06:50:54 +0200

    Delta assertions documentation updated.

--- emunit_assertions_delta.h
+++ emunit_assertions_delta.h
@@ -112,12 +112,12 @@
  *
  * @sa EMUNIT_CONF_NUMBER_SIZE
  *
- * @param[in] delta
+ * @param[in] d Delta
  * @param[in] e Expected value
  * @param[in] a Actual value
  */
 /**
- * @def UT_ASSERT_DELTA_UINT(e, a)
+ * @def UT_ASSERT_DELTA_HEX(d, e, a)
  * @brief Default unsigned integer assertion with delta - hexadecimal display
  *
  * @copydetails UT_ASSERT_DELTA_UINT
@@ -129,7 +129,53 @@
 #define UT_ASSERT_DELTA_HEX( d, e, a) EMUNIT_CN2(UT_ASSERT_DELTA_HEX,  EMUNIT_CONF_NUMBER_SIZE)(d, e, a)
 
 
-/** @todo Documentation */
+/**
+ * @def UT_ASSERT_DELTA_MSG(d, e, a, ...)
+ * @brief Default integer with delta assertion with message
+ *
+ * This macro maps directly to @ref UT_ASSERT_DELTA_INT_MSG
+ *
+ * @param[in] d   Delta
+ * @param[in] e   Expected value
+ * @param[in] a   Actual value
+ * @param[in] ... Format string followed by the format values.
+ *                Standard @c printf format is used.
+ */
+/**
+ * @def UT_ASSERT_DELTA_INT_MSG(d, e, a, ...)
+ * @brief Default signed integer with delta assertion with message
+ *
+ * Macro that checks signed integer value with maximum allowed size.
+ *
+ * @sa EMUNIT_CONF_NUMBER_SIZE
+ *
+ * @param[in] d   Delta
+ * @param[in] e   Expected value
+ * @param[in] a   Actual value
+ * @param[in] ... Format string followed by the format values.
+ *                Standard @c printf format is used.
+ */
+/**
+ * @def UT_ASSERT_DELTA_UINT_MSG(d, e, a, ...)
+ * @brief Default unsigned integer with delta assertion with message
+ *
+ * Macro that checks unsigned integer value with maximum allowed size.
+ *
+ * @sa EMUNIT_CONF_NUMBER_SIZE
+ *
+ * @param[in] d   Delta
+ * @param[in] e   Expected value
+ * @param[in] a   Actual value
+ * @param[in] ... Format string followed by the format values.
+ *                Standard @c printf format is used.
+ */
+/**
+ * @def UT_ASSERT_DELTA_HEX_MSG(d, e, a, ...)
+ * @brief Default unsigned integer assertion with delta, message version - hexadecimal display
+ *
+ * @copydetails UT_ASSERT_DELTA_UINT_MSG
+ */
+
 #define UT_ASSERT_DELTA_MSG(d, e, a, ...)      UT_ASSERT_DELTA_INT_MSG(d, e, a, __VA_ARGS__)
 #define UT_ASSERT_DELTA_INT_MSG( d, e, a, ...) EMUNIT_CN3(UT_ASSERT_DELTA_INT,  EMUNIT_CONF_NUMBER_SIZE, _MSG)(d, e, a, __VA_ARGS__)
 #define UT_ASSERT_DELTA_UINT_MSG(d, e, a, ...) EMUNIT_CN3(UT_ASSERT_DELTA_UINT, EMUNIT_CONF_NUMBER_SIZE, _MSG)(d, e, a, __VA_ARGS__)
@@ -156,7 +202,7 @@
  * @def UT_ASSERT_DELTA_INT32(d, e, a)
  * @brief Signed 32-bit assertion with delta
  *
- * @copydetails UT_ASSERT_DELTA_BOOL
+ * @copydetails UT_ASSERT_DELTA_INT8
  *
  *  @note This assertion may be disabled.
  *        See @ref EMUNIT_CONF_NUMBER_SIZE for details.
@@ -231,7 +277,90 @@
 #define UT_ASSERT_DELTA_HEX32( d, e, a)  UT_ASSERT_DELTA_x(u, uint32_t, EMUNIT_NUMTYPE_X32, (d), (e), (a))
 #define UT_ASSERT_DELTA_HEX64( d, e, a)  UT_ASSERT_DELTA_x(u, uint64_t, EMUNIT_NUMTYPE_X64, (d), (e), (a))
 
-/** @todo Documentation */
+/**
+ * @def UT_ASSERT_DELTA_INT8_MSG(d, e, a, ...)
+ * @brief Signed 8-bit assertion with delta, version with message
+ *
+ * Checks if actual value is between expected +/- delta.
+ *
+ * @param[in] d Delta
+ * @param[in] e Expected value
+ * @param[in] a Actual value
+ * @param[in] ... Format string followed by the format values.
+ *                Standard @c printf format is used.
+ */
+/**
+ * @def UT_ASSERT_DELTA_INT16_MSG(d, e, a, ...)
+ * @brief Signed 16-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT8_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_INT32_MSG(d, e, a, ...)
+ * @brief Signed 32-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT8_MSG
+ *
+ *  @note This assertion may be disabled.
+ *        See @ref EMUNIT_CONF_NUMBER_SIZE for details.
+ */
+/**
+ * @def UT_ASSERT_DELTA_INT64_MSG(d, e, a, ...)
+ * @brief Signed 64-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT32_MSG
+ */
+
+/**
+ * @def UT_ASSERT_DELTA_UINT8_MSG(d, e, a, ...)
+ * @brief Unsigned 8-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT8_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_UINT16_MSG(d, e, a, ...)
+ * @brief Unsigned 16-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT16_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_UINT32_MSG(d, e, a, ...)
+ * @brief Unsigned 32-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT32_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_UINT64_MSG(d, e, a, ...)
+ * @brief Unsigned 64-bit assertion with delta, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT64_MSG
+ */
+
+/**
+ * @def UT_ASSERT_DELTA_HEX8_MSG(d, e, a, ...)
+ * @brief Unsigned 8-bit assertion with delta - hexadecimal display, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT8_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_HEX16_MSG(d, e, a, ...)
+ * @brief Unsigned 16-bit assertion with delta - hexadecimal display, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT16_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_HEX32_MSG(d, e, a, ...)
+ * @brief Unsigned 32-bit assertion with delta - hexadecimal display, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT32_MSG
+ */
+/**
+ * @def UT_ASSERT_DELTA_HEX64_MSG(d, e, a, ...)
+ * @brief Unsigned 64-bit assertion with delta - hexadecimal display, version with message
+ *
+ * @copydetails UT_ASSERT_DELTA_INT64_MSG
+ */
+
 #define UT_ASSERT_DELTA_INT8_MSG(  d, e, a, ...)  UT_ASSERT_DELTA_MSG_x(s, int8_t,   EMUNIT_NUMTYPE_S8,  (d), (e), (a), __VA_ARGS__)
 #define UT_ASSERT_DELTA_INT16_MSG( d, e, a, ...)  UT_ASSERT_DELTA_MSG_x(s, int16_t,  EMUNIT_NUMTYPE_S16, (d), (e), (a), __VA_ARGS__)
 #define UT_ASSERT_DELTA_INT32_MSG( d, e, a, ...)  UT_ASSERT_DELTA_MSG_x(s, int32_t,  EMUNIT_NUMTYPE_S32, (d), (e), (a), __VA_ARGS__)
